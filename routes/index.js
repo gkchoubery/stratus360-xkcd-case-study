a1b98c75ca21d470c5608ccd13c2f9bad2ca3c4a
Geet Choubey <geetchoubey@gmail.com>
2021-06-08 16:13:30 -0400

    Added random button

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,43 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const visitCount = require('../visitCount');
+const visitCount = require('../utils/visitCount');
+const random = require('../utils/random');
 
-const BASE_URL = 'https://xkcd.com/';
+const BASE_URL = 'https://xkcd.com';
 
 const api = axios.create({
   baseURL: BASE_URL
 });
 
-router.get('/', async (req, res, next) => {
-  res.redirect('/1');
+const MAX_COMICS = 2473;
+
+function parseComic(data) {
+  const { num } = data;
+  const left = (num - 1) > 0 ? num - 1 : false;
+  const right = num < MAX_COMICS ? num + 1 : false;
+  return {
+    left, right,
+    visits: visitCount(num),
+    ...data
+  };
+}
+
+router.get('/', async (req, res) => {
+  const result = (await api.get(`/info.0.json`)).data;
+  res.render('comic', parseComic(result));
+});
+
+router.get('/random', (req, res) => {
+  res.redirect(`/${random(MAX_COMICS)}`);
 });
 
 router.get('/:id', async (req, res, next) => {
   let { id } = req.params;
   try {
     id = parseInt(id);
-    const result = (await api.get(`${id}/info.0.json`)).data;
-    const left = (id - 1) > 0 ? id - 1 : false;
-    const right = id + 1;
-    const data = {
-      left, right, 
-      visits: visitCount(id),
-      ...result
-    };
-    res.render('comic', data);
+    const result = (await api.get(`/${id}/info.0.json`)).data;
+    res.render('comic', parseComic(result));
   } catch (e) {
     next();
   }
